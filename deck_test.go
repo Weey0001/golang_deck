3dc2f9f408fb84381ff233d290f1e3ddf7f7dc2f
andrianantara0001 <andrian.andriamb@gmail.com>
2023-11-16 01:25:25 +0200

    test in go

---
+++ deck_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+// 't.Errorf("Expected deck length of 16, but got", len(d))
+
+// to this:
+
+// t.Errorf("Expected deck length of 16, but got %v", len(d))'
+
+func TestNewDeck(t *testing.T){
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+
+	}
+}
