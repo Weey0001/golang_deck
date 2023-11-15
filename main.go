64f6472406e915b28e9a2e1a71db4b65eb9992ed
andrianantara0001 <andrian.andriamb@gmail.com>
2023-11-15 23:22:47 +0200

    read and write file

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	cards := newDeck()
-
-	cards.saveToFile("my_cards")
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
 
 	// hand, remainingcards := deal(cards, 5)
 	// hand.print()
 	// remainingcards.print()
+
+	cards := newDeckFromFile("my_cards")
+	fmt.Println(cards)
 }
